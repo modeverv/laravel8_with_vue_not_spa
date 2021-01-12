6d0a3a93f3c5919d3be31916552f2367686f25a4
池畑成二郎 <seijiro@seijiro-MBP.local>
2021-01-13 02:46:33 +0900

    initial

---
+++ webpack.mix.js
@@ -0,0 +1,31 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+mix
+    .js('resources/js/app.js', 'public/js')
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css')
+    .browserSync({
+        proxy: {
+        target: "http://homestead.test" // 最後に/は不要
+     },
+     https: false, // httpsのサイトをproxyするならtrueをセット
+     files: [ // チェックするファイルは下記で十分ではないかな。
+       './resources/**/*',
+       './app/**/*',
+       './config/**/*',
+       './routes/**/*',
+       './public/**/*'
+     ],
+     //open: false, //BrowserSync起動時にブラウザを開かない
+     reloadOnRestart: true //BrowserSync起動時にブラウザにリロード命令おくる
+  });
